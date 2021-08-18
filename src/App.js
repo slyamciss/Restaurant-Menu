74903c2bd88684c37bd0818b4f702ebee3296201
Souleymane Cisse <slyamciss@gmail.com>
2021-08-18 18:11:46 -0400

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+function App() {
+  const [menuItems, setMenuItems] = useState();
+  return (
+    <div>
+      <h1>Tariq Component</h1>
+    </div>
+  );
+}
+
+export default App;

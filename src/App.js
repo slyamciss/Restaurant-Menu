cd3538eba04011a7139ce6b4c80dc7694d85937e
Souleymane Cisse <slyamciss@gmail.com>
2021-08-18 21:49:42 -0400

    final touched

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
 function App() {
-  const [menuItems, setMenuItems] = useState();
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
-    <div>
-      <h1>Tariq Component</h1>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
